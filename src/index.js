0d2978f44e09a0abf06874c3ce7fbbfd1c15f3ae
Johann Guayacundo Avellaneda <johann.guayacundo@rapyuta-robotics.com>
2022-08-18 16:09:28 +0900

    Removed contexts from action

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const core = require('@actions/core');
-const markup = require("markup-js");
-const _ = require('lodash');
 
-const contextNames = ['github', 'job', 'runner', 'strategy', 'matrix'];
 const mappingFormat = /(.+?)="(.+?)"/;
 
-const performAdHocReplacements = (replacements, data) => {
-    let out = data;
+const run = async () => {
+    const inputFile = core.getInput('template-input-path');
+    const outputFile = core.getInput('template-output-path');
+    const replacements = core.getInput('replacements')
+    let data = fs.readFileSync(path.resolve(inputFile), 'utf-8');
     let map = new Map();
-    let arr = replacements.split('\n');
     let regex = new RegExp(mappingFormat);
-    arr.forEach((replacement) => {
+    replacements.split('\n').forEach((replacement) => {
         let match = regex.exec(replacement);
         if (match.length == 3) {
             map.set(`%${match[1]}`, match[2]);
@@ -21,30 +20,8 @@ const performAdHocReplacements = (replacements, data) => {
         }
     });
     map.forEach((key, val) => {
-        out = out.replace(val, key);
+        data = out.replace(val, key);
     });
-    return out;
-}
-
-const run = async () => {
-    const context = {};
-    const inputFile = core.getInput('template-input-path');
-    const outputFile = core.getInput('template-output-path');
-    contextNames.forEach(name => {
-        try {
-            let json = core.getInput(`${name}-context`);
-            _.set(context, name, JSON.parse(json));
-        } catch (error) {
-            core.warning(`Unable to read ${name} context: ${error}`);
-        }
-    });
-    let data = fs.readFileSync(path.resolve(inputFile), 'utf-8');
-    data = data.replace(/\$\{\{/g, '{{');
-    data = markup.up(data, context);
-    let adHocReplacements = core.getInput('ad-hoc-replacements');
-    if (adHocReplacements) {
-        data = performAdHocReplacements(adHocReplacements, data);
-    }
     fs.writeFileSync(path.resolve(outputFile), data);
     core.setOutput('template-output-path', outputFile);
 }
