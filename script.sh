a5f9ad34806f3f8263a505ee4be6d3fedbbd49c6
Johann Guayacundo Avellaneda <johann.guayacundo@rapyuta-robotics.com>
2022-08-19 18:36:58 +0900

    Corrected usage of input variable, removed folders

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 IFS=$'\n'
 lines=($REPL)
 tmp=$(cat $1)
@@ -7,6 +7,6 @@ for l in "${lines[@]}"
 do
     key=$(echo $l | sed -e "s/\(.\+\)=\".\+\"/\1/g")
     val=$(echo $l | sed -e "s/.\+=\"\(.\+\)\"/\1/g")
-    tmp=$(sed -e "s~%$key~$val~g" <<< $tmp)
+    tmp=$(sed -e "s~%$key~$val~g" <<< $tmp) || echo "Failed replacement for $l"
 done
 echo "$tmp" > $1.fixed
