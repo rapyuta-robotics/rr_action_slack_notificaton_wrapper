5852aa1978bbb80f12416de2e2a284f019efed96
Johann Guayacundo Avellaneda <johann.guayacundo@rapyuta-robotics.com>
2022-08-19 16:42:42 +0900

    Corrected usage of template fixer script

--- script.sh
+++ script.sh
@@ -2,10 +2,11 @@
 
 IFS=$'\n'
 lines=($1)
-dl=$(echo -en "\001");
+tmp=$(cat $2)
 for l in "${lines[@]}"
 do
     key=$(echo $l | sed -e "s/\(.\+\)=\".\+\"/\1/g")
     val=$(echo $l | sed -e "s/.\+=\"\(.\+\)\"/\1/g")
-    sed -e "s~%$key~$val~g" -i $2 || echo "Replacement failed for $l"
+    tmp=$(sed -e "s~%$key~$val~g" <<< $tmp)
 done
+echo "$tmp"
