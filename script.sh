c34344b35952038a220a2fac50b16511f5bcb4ab
Johann Guayacundo Avellaneda <johann.guayacundo@rapyuta-robotics.com>
2022-09-12 18:35:29 +0900

    Added line debubbing

--- script.sh
+++ script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 IFS=$'\n'
-lines=($(echo $REPL | grep -Po "[A-Za_z0-9-_]+=\".+?\""))
+lines=($(echo $REPL))
 tmp=$(cat $1)
 for l in "${lines[@]}"
 do
+    echo $l
+    echo "*************************"
     key=$(echo $l | sed -e "s/\(.\+\)=\".\+\"/\1/g")
     val=$(echo $l | sed -e "s/.\+=\"\(.\+\)\"/\1/g")
     tmp=$(sed -e "s~%$key~$val~g" <<< $tmp) || echo "Failed replacement for $l"
